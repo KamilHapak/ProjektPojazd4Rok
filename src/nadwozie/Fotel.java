01ac3d4ac11f7e230b1409204b3e833b3c5255d0
AldurilStudy <skop.pawel1@gmail.com>
2014-10-30 08:57:02 +0100

    Dodano klasy ChoinkaZapachowa i Fotel

---
+++ src/nadwozie/Fotel.java
@@ -0,0 +1,16 @@
+
+package nadwozie;
+
+/**
+ * Klasa służy do reprezentowania fotela.
+ * @author Paweł Skop
+ */
+public class Fotel {
+    
+    /**
+     * Tworzy obiekt klasy Fotel.
+     */
+    public  Fotel(){
+    System.out.println("[nadwozie.Fotel] Utworzono obiekt klasy Fotel");
+    }//public Fotel
+}//public class Fotel
