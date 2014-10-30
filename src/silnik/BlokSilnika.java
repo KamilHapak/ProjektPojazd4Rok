7742361bedf32cfd9271471e9198e157d9002415
Piotr Paluch <peter88p@gmail.com>
2014-10-30 08:59:43 +0100

    Dodałem klasy: BlokSilnika i Szyberdach

---
+++ src/silnik/BlokSilnika.java
@@ -0,0 +1,16 @@
+package silnik;
+
+/**
+ * Klasa służy do reprezentowania silnika w pojeździe
+ * @author Piotr Paluch
+ */
+public class BlokSilnika {
+    
+    /**
+     * Tworzy obiekt klasy BlokSilnika
+     */
+    public BlokSilnika() {
+        System.out.println("[silnik.BlokSilnika] Utworzono obiekt klasy BlokSilnika");
+    }//public BlokSilnika
+    
+} //public class BlokSilnika
