d44f38243416115e705c830f0c442e9810d846b2
Sebastian Klimiuk <sebastiak.k.klimiuk@gmail.com>
2014-10-30 09:24:49 +0100

    Dodano klase Opona i Felga

---
+++ src/silnik/FiltrPowietrza.java
@@ -0,0 +1,16 @@
+
+package silnik;
+
+/**
+ * Klasa służy do reprezentowania filtru powietrza w pojeździe.
+ * @author dmar625
+ */
+public class FiltrPowietrza {
+    /**
+     * Tworzy obiekt klasy FiltrPowietrza
+     */
+    
+    public FiltrPowietrza() {
+     System.out.println("[silnik.FiltrPowietrza] Utworzono obiekt klasy FiltrPowietrza");
+    }// public FiltrPowietrza
+}//public class FiltrPowietrza
