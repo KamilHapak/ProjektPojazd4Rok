2129a26a2f4f16c352ed78ff4330145ab9884248
Kamil Hapak <kamil.hapak@gmail.com>
2014-11-05 22:23:32 +0100

    Dodanie klasy Alternator

---
+++ src/ukladElektryczny/Alternator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ukladElektryczny;
+
+/**
+ * Klasa reprezentująca alternator w pojeździe
+ *
+ * @author Kamil Hapak
+ */
+public class Alternator {
+
+    
+    /*
+    * Konstruktor klasy Alternator
+    */
+    public Alternator() {
+        System.out.println("[ukladElektruczny.Alternator] Utworzono obiekt klasy alternator.");
+    }//public Alternator
+}
