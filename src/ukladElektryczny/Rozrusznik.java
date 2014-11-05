f528191129b90e622f1cb5c9f8d844aab26bff9f
Kamil Hapak <kamil.hapak@gmail.com>
2014-11-05 22:28:30 +0100

    Dodanie klasy Rozrusznik

---
+++ src/ukladElektryczny/Rozrusznik.java
@@ -0,0 +1,17 @@
+package ukladElektryczny;
+
+/**
+ * Klasa reprezentująca rozrusznik w pojeździe
+ *
+ * @author Kamil Hapak
+ */
+public class Rozrusznik {
+
+    
+    /*
+    * Konstruktor klasy Alternator
+    */
+    public Rozrusznik() {
+        System.out.println("[ukladElektruczny.Rozrusznik] Utworzono obiekt klasy rozrusznik.");
+    }//public Rozrusznik
+}
